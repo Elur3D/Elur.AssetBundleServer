b08856646788f36246898097eeb729420ade1fe2
Jon Ander Hernandez <jonan.h@gmail.com>
2016-07-19 22:54:30 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    multer  = require('multer')
+
+var app = express()
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.post('/',[ multer({ dest: './uploads/'}), function(req, res){
+    console.log(req.body) // form fields
+    console.log(req.files) // form files
+    res.status(204).end()
+}]);
+
+app.listen(3000);
